5d278069ee0604f96cdf26033c37bd64d4b70f9e
Laura Thomas <lthomas@edcoe.org>
2019-07-12 14:15:31 -0700

    Add project files.

---
+++ practicing-git-bash-2/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace practicing_git_bash_2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ practicing-git-bash-2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="practicing_git_bash_2.MvcApplication" Language="C#" %>
